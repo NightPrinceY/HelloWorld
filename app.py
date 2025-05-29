aa779f91ba84f0c8ee55e98370c5ea9133257112
NightPrince <yahyaalnwsany39@gmail.com>
2025-05-29 22:08:27 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '''
+    <html>
+        <head>
+            <title>Hello</title>
+            <style>
+                body {
+                    background-color: black;
+                    color: white;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    margin: 0;
+                    font-family: Arial, sans-serif;
+                }
+                h1 {
+                    font-size: 64px;
+                    text-align: center;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Hello, World!</h1>
+        </body>
+    </html>
+    '''
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7860)
